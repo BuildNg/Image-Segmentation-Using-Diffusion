4e9152240a978f0c18bcdfdc7f81471fe3c73b23
Dung Tran <dung20042911@gmail.com>
2026-02-17 22:18:52 -0500

    New  training config

--- LDSeg-mod/LDSeg_mod.py
+++ LDSeg-mod/LDSeg_mod.py
@@ -99,11 +99,13 @@ class DistEncoder(nn.Module):
                 layers.append(nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True))
 
             layers.append(nn.Conv2d(in_dim, out_dim, kernel_size=3, padding=int(padding)))
-            layers.append(nn.ReLU(inplace=True))
+            layers.append(nn.BatchNorm2d(out_dim))
+            layers.append(nn.SiLU(inplace=True))
 
             for _ in range(no_convs_per_block - 1):
                 layers.append(nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=int(padding)))
-                layers.append(nn.ReLU(inplace=True))
+                layers.append(nn.BatchNorm2d(out_dim))
+                layers.append(nn.SiLU(inplace=True))
 
         self.layers = nn.Sequential(*layers)
         self.layers.apply(init_weights)
